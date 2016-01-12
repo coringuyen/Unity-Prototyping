b0cec60e8b176ba382135011d1b3abae7806c34c
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-01-12 15:16:30 -0600

    showcase scene and controller input

---
+++ FF-Prototype/Assets/Scripts/InputManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject ActiveModel;
+
+    public ShowcaseUI GUIManager;
+
+    bool time;
+    float animTimer = 0;
+
+    public bool MenuDirty = false;
+
+    void Update()
+    {
+        AnimationControls();
+        GeneralControls();
+        Timer();
+    }
+
+    void Timer()
+    {
+        if(time == true)
+        {
+            animTimer += Time.deltaTime * 1;
+        }
+    }
+
+    void GeneralControls()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton7))
+        {
+            GUIManager.PauseMenu.SetActive(!MenuDirty);
+            MenuDirty = !MenuDirty;
+            GUIManager.MenuActivated(GUIManager.PauseMenu);
+        }
+    }
+
+    void AnimationControls()
+    {
+        if (MenuDirty != true)
+        {
+            //Jab
+            if (Input.GetKeyDown(KeyCode.JoystickButton0))
+            {
+                ActiveModel.GetComponent<Animator>().SetBool("Jab", true);
+            }
+            if (Input.GetKeyDown(KeyCode.JoystickButton1))
+            {
+                ActiveModel.GetComponent<Animator>().SetBool("Hikick", true);
+            }
+            if (Input.GetKeyDown(KeyCode.JoystickButton8))
+            {
+                ActiveModel.GetComponent<Animator>().SetBool("Run", true);
+                time = true;
+            }
+            if (animTimer >= 5)
+            {
+                ActiveModel.GetComponent<Animator>().SetBool("Run", false);
+                time = false;
+                animTimer = 0;
+            }
+        }
+
+    }
+}
---
+++ FF-Prototype/Assets/Scripts/ShowcaseUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ShowcaseUI : MonoBehaviour
+{
+    public GameObject InfoMenu;
+        public List<Text> infoText;
+
+    public GameObject PauseMenu;
+        public List<Button> pauseButtons;
+
+    public GameObject ControlsMenu;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MenuActivated(GameObject menu)
+    {
+        if(menu == PauseMenu)
+        {
+            
+        }
+    }
+}
