b84104ea059ffd2011cae16194f712d1f3c669ec
chuwilliamson <chuwilliamson@gmail.com>
2016-06-02 19:29:16 -0500

    slowmo and hitboxes

---
+++ FF-Prototype/Assets/Scripts/HitBoxTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class HitEvent : UnityEvent
+{
+
+}
+[RequireComponent(typeof(BoxCollider))]
+public class HitBoxTrigger : MonoBehaviour
+{
+    public static HitEvent EventHit;    
+    void Awake()
+    {
+        if (EventHit == null)
+            EventHit = new HitEvent();
+        EventHit.AddListener(BroadcastHit);
+    }
+
+    void BroadcastHit()
+    {
+        print("hit");
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+        {            
+            if(EventHit != null)
+                EventHit.Invoke();
+        }
+    }
+}
+
