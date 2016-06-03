53fce2b2c5a36ae48c9ad41baeb295eba11488cd
coringuyen <coringuyen96@gmail.com>
2016-06-03 18:10:17 -0500

    play with GUI button
    when character in game. All the GUI elements for the character should be
    connected

---
+++ FF-Prototype/Assets/Scripts/AbilitiesGUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AbilitiesGUI : MonoBehaviour
+{
+    public Button attack, defend, endTurn;
+    public Text aveVel, insVel;
+    private GameObject player;
+    public Transform Origin;
+    void Start ()
+    {
+        StartCoroutine(FindPlayer());
+    }
+
+    IEnumerator FindPlayer()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        Transform target = player.GetComponent<UnitMovement>().target;
+
+        attack.onClick.AddListener(delegate { player.GetComponent<UnitMovement>().Action(target); });
+        endTurn.onClick.AddListener(delegate { player.GetComponent<UnitMovement>().Action(Origin); });
+
+        player.GetComponent<UnitMovement>().avgVelocity = aveVel;
+        player.GetComponent<UnitMovement>().insVelocity = insVel;
+
+        yield return new WaitForSeconds(0.0f);
+    }
+}
